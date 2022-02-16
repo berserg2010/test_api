63b63e1c8a1eb51b9aee8f4493340909ebff21f1
Sergei Zakamskov <berserg2010@gmail.com>
2022-02-16 18:39:20 +0300

    Test manual workflow

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from decouple import config
@@ -91,7 +92,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static_src'),
+)
+
+STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
